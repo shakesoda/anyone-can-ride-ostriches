2a1fef26130911fa242dfd00e9e91df2ee6a4f54
Colby Klein <shakesoda@gmail.com>
2013-09-12 22:15:01 -0700

    Implement basic commands needed for an acro game
    featuring AJ on Gobby

--- server/acro.lua
+++ server/acro.lua
@@ -1,47 +1,10 @@
-local acro = {}
+acro = {}
 
 -- game settings
-acro.settings = {
-	mode = "normal",
-	normal = {
-		player_limit = 15,
-		score_limit = 50,
-		min_length = 2,
-		max_length = 5,
-		super_length = 10,
-		allow_cockblock = false,
-		allow_communism = false,
-		allow_super = false
-	},
-	turkeyslam = {
-		player_limit = 15,
-		score_limit = 75,
-		min_length = 2,
-		max_length = 5,
-		super_length = 10,
-		allow_cockblock = true, -- you're not allowed to win
-		allow_communism = true, -- fuck you! freem gets the point instead
-		allow_super = true -- super 10 letter rounds
-	}
-}
-
+acro.settings = require "./server/settings"
+acro.hooks = { normal = {} }
 acro.players = {}
 acro.scores = {}
-acro.acros = {}
-
---[[
--- some utf8 things that might be useful
--- http://stackoverflow.com/questions/13235091
--- get first letter
-function firstLetter(str)
-  return str:match("[%z\1-\127\194-\244][\128-\191]*")
-end
-
--- utf8 iter
-for code in str:gmatch("[%z\1-\127\194-\244][\128-\191]*") do
-  print(code)
-end
-]]
 
 -- http://stackoverflow.com/questions/1426954
 function string:split(pat)
@@ -54,12 +17,38 @@ function string:split(pat)
 	return function() if st then return getter(st, g()) end end
 end
 
+function acro:add_player(name)
+	-- don't nuke a player if they join again (disconnects and shit)
+	if self.players[name] then
+		self.players[name].active = true
+		return
+	end
+
+	self.players[name] = {}
+	self.players[name].active = true
+	self.players[name].score = 0
+end
+
+function acro:remove_player(name)
+	-- don't clear score until end of round in case they disconnected
+	if self.players[name] then
+		self.players[name].active = false
+	end
+end
+
 function acro:validate(text)
 	local count = 0
 	local errors = {}
+	-- TODO: probably use some error codes and let the various modes change messages
+	-- also needed for penalties.
+	for player, acro in pairs(self.acros) do
+		if text == acro.text then
+			self.acros[acro.player] = nil -- filthy casuals
+			table.insert(errors, "you idiots submitted the same acros like a bunch of xxxtacos!")
+		end
+	end
 	for token in text:split() do
 		count = count + 1
-		--print(token:sub(1,1), self.current_acro:sub(count,count))
 		if count > #self.current_acro then
 			table.insert(errors, "too many words!")
 			break
@@ -74,7 +63,25 @@ function acro:validate(text)
 	return #errors == 0, errors
 end
 
-function acro:generate_acro()
+function acro:submit_acro(name, text)
+	local valid, errors = acro:validate(text)
+	if valid then
+		self.acros[name] = { text=text, idx=0, score=0, player=name }
+	else
+		print ("Look how many ways you've fucked up, " .. name .. ":")
+		for i, v in ipairs(errors) do
+			print(i .. ": "..v)
+		end
+	end
+end
+
+function acro:generate_acro(manual)
+	-- mostly for debugging
+	if manual then
+		self.current_acro = manual
+		return
+	end
+
 	-- TODO: weight specific letters so you don't get bullshit acro's all the time
 	local settings = self.settings[self.settings.mode]
 	self.current_acro = ""
@@ -85,22 +92,91 @@ function acro:generate_acro()
 	end
 end
 
--- testing
-acro:generate_acro()
-local valid, errors = acro:validate("this answer is postmodern")
-if valid then
-	print "valid acro what do you know"
-	-- acro.acros[player, text]
-else
-	for k,v in pairs(errors) do
-		print(v)
+function acro:begin_round(manual)
+	local settings = self.settings[self.settings.mode]
+
+	self.acros = {}
+	self.votes = {}
+
+	self:generate_acro(manual)
+	-- TODO: put begin vote on timer
+
+	print() -- gimme some space
+	print("The acro for this round is "..self.current_acro..". Voting begins in "..settings.time_limit.." seconds.")
+end
+
+function acro:end_round()
+	-- TODO: declare winner, fuck with points.
+	local winner = { score = 0, text = "everyone loses" }
+	for _, acro in pairs(self.acros) do
+		if acro.score > winner.score then
+			winner = acro
+		end
+		if not self.scores[acro.player] then
+			self.scores[acro.player] = 0
+		end
+		self.scores[acro.player] = self.scores[acro.player] + acro.score
+		print(acro.player .. "'s acro: "..acro.text.." (".. acro.score .." votes)")
+	end
+
+	if winner.player == nil then
+		print("Nobody won! What a bunch of losers!")
+	else
+		print("The winner for this round is "..winner.player.."!")
+	end
+
+	print("Scores:")
+	for player, score in pairs(self.scores) do
+		print(player .. ": " .. score)
 	end
 end
 
--- wait a while
+function acro:list_acros()
+	local settings = self.settings[self.settings.mode]
+	local idx = 1
+	for name, acro in pairs(self.acros) do
+		local player = self.players[name]
+		acro.idx = idx
+		local outLine = idx .. ": " .. acro.text
+		if player.score > settings.score_limit * 0.9 and math.random(1,3) == 2 then
+			outLine = outLine .. " (btw this is "..player.."'s acro)"
+		end
+		print(outLine)
+		idx = idx + 1
+	end
+end
 
--- judge round
+function acro:begin_vote()
+	if next(self.acros) == nil then
+		print "Nobody submitted anything!"
+		return
+	end
 
--- distribute points
+	for player, acro in pairs(self.acros) do
+		-- TODO: shuffle acro list and store the IDs
+	end
 
--- check if anyone passed point threshold
+
+	self:list_acros()
+
+	-- TODO: put end round on timer
+end
+
+
+function acro:vote(voter, id)
+	-- TODO: dq acro if someone votes for themselves like an asshole
+	-- usually it's just a message and no DQ, but it could be an option
+	for player, acro in pairs(self.acros) do
+		if acro.idx == id then
+			if acro.player == voter then
+				print "You can't vote for yourself!"
+			else
+				if not self.votes[id] then
+					self.votes[id] = acro
+				end
+				self.votes[id].score = self.votes[id].score + 1
+			end
+			break
+		end
+	end
+end
---
+++ server/settings.lua
@@ -0,0 +1,25 @@
+return {
+	mode = "normal",
+	normal = {
+		player_limit = 15,
+		score_limit = 50,
+		time_limit = 60,
+		min_length = 2,
+		max_length = 5,
+		super_length = 10,
+		allow_cockblock = false,
+		allow_communism = false,
+		allow_super = false
+	},
+	turkeyslam = {
+		player_limit = 15,
+		score_limit = 75,
+		time_limit = 60,
+		min_length = 2,
+		max_length = 5,
+		super_length = 10,
+		allow_cockblock = true, -- you're not allowed to win
+		allow_communism = true, -- fuck you! freem gets the point instead
+		allow_super = true -- super 10 letter rounds
+	}
+}
