6fecdd82e26abdb8486d1a5eccfb3ae8896cfd65
AJ Kelly <aj187@ajworld.net>
2013-09-14 12:26:08 -0500

    added super mode

--- games/settings.lua
+++ games/settings.lua
@@ -2,7 +2,7 @@ local settings = {
 	game = "acro",
 	mode = "normal",
 
-	-- rulesets
+	-- rulesets (Acro)
 	normal = {
 		time_limit = 60,
 		voting_time_limit = 30,
@@ -19,6 +19,24 @@ local settings = {
 			A = 6, B = 4, C = 3, D = 2, E = 2, F = 3, G = 2, H = 3, I = 6,
 			J = 2, K = 1, L = 2, M = 3, N = 2, O = 2, P = 2, Q = 1, R = 3,
 			S = 6, T = 6, U = 1, V = 1, W = 3, X = 1, Y = 3, Z = 1
+		}
+	},
+	super = {
+		time_limit = 90,
+		voting_time_limit = 60,
+		time_between_rounds = 60,
+		player_limit = 15,
+		score_limit = 51,
+		min_length = 3,
+		max_length = 7,
+		super_length = 10,
+		allow_cockblock = false,
+		allow_communism = false,
+		allow_super = true,
+		weights = {
+			A = 6, B = 3, C = 3, D = 2, E = 2, F = 3, G = 2, H = 3, I = 6,
+			J = 1, K = 2, L = 2, M = 3, N = 2, O = 3, P = 2, Q = 1, R = 3,
+			S = 6, T = 6, U = 1, V = 2, W = 3, X = 1, Y = 3, Z = 1
 		}
 	},
 	turkeyslam = {
